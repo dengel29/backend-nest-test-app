7f9cb0a26a81cbe9a8f2c2a510bc29d188d86467
Dan Engel <dengel29@gmail.com>
2024-06-07 17:39:47 +0800

    packages installed, ormconfig created, typeorm recipe pulled into app.module

---
+++ src/ormconfig.ts
@@ -0,0 +1,22 @@
+import { registerAs } from '@nestjs/config';
+import { config as dotenvConfig } from 'dotenv';
+import { join } from 'path';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+dotenvConfig({ path: '.env' });
+
+const config = {
+  type: 'postgres',
+  host: `${process.env.DB_HOST}`,
+  port: `${process.env.DB_PORT}`,
+  username: `${process.env.DB_USERNAME}`,
+  password: `${process.env.DB_PASSWORD}`,
+  database: `${process.env.DB_DATABASE}`,
+  migrations: [join(__dirname, '../src/db/migrations/*{.ts,.js}')],
+  entities: ['../**/*.entity{.ts,.js}'],
+  autoLoadEntities: false,
+  synchronize: true,
+};
+
+export default registerAs('typeorm', () => config);
+export const connectionSource = new DataSource(config as DataSourceOptions);
